7040bf7a69ebedd0140251eb88af96c02bfe8e97
José Wilson Aragão <josewilsonaj@gmail.com>
2026-07-17 19:04:23 -0300

    Update AuthContext.jsx

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -5,7 +5,6 @@ export const AuthContext = createContext({})
 export function AuthProvider({ children }) {
   const [usuario, setUsuario] = useState(null)
 
-  // Carrega o usuário do localStorage assim que o app inicia
   useEffect(() => {
     const usuarioSalvo = localStorage.getItem("usuario")
     if (usuarioSalvo) {
@@ -13,18 +12,16 @@ export function AuthProvider({ children }) {
     }
   }, [])
 
-  // Função para fazer login global
   const loginGlobal = (dadosUsuario) => {
     localStorage.setItem("usuario", JSON.stringify(dadosUsuario))
     localStorage.setItem("usuarioId", dadosUsuario.id)
-    setUsuario(dadosUsuario) // Atualiza o estado e avisa todos os componentes
+    setUsuario(dadosUsuario) 
   }
 
-  // Função para fazer logout global
   const logoutGlobal = () => {
     localStorage.removeItem("usuario")
     localStorage.removeItem("usuarioId")
-    setUsuario(null) // Reseta o estado instantaneamente
+    setUsuario(null) 
   }
 
   return (
